4bb8d6c1dcdfe134211c2a15b9db49b22845d625
luwickX3 <124098586+luwickX3@users.noreply.github.com>
2023-08-21 08:50:16 -0600

    Add files via upload

---
+++ InterOP/InterOP/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.Office.Interop.Excel
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ExcelApp = New Microsoft.Office.Interop.Excel.Application
+        Dim Libro = ExcelApp.Workbooks.Add
+
+        Libro.Sheets(1).Cells(1, 1) = "hola mundo"
+        Libro.SaveAs(Filename:="C:\Users\Rodrigo Alexis\Escritorio\Test.xlsx")
+
+        ExcelApp.Quit()
+
+        Libro = Nothing
+        ExcelApp = Nothing
+    End Sub
+End Class
